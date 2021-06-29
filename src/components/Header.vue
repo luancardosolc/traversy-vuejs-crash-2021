3e896ff34a37ba8bc1b46ac76584d5d48792ab43
Luan Cardoso <luanleitecardoso@gmail.com>
2021-06-28 22:21:42 -0300

    Button to task adding with color bind

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button text="Add Task" color="green"/>
+  </header>
+</template>
+
+<script>
+import Button from './Button.vue';
+
+export default {
+  name: 'Header',
+  props: {
+    title: String
+  },
+  components: {
+    Button
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+</style>
